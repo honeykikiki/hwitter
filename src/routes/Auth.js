b8d5dd1123308deb7c9a715ccbaad52a74159645
honeykikiki <tjdwls75666@naver.com>
2021-11-01 18:13:25 +0900

    firebase 구글 로그인 깃험 로그인 기능 공부

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import { authService } from 'fbase';
+import React, { useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccoutn, setNewAccount] = useState(true);
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    let data;
+    try {
+      if (newAccoutn) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password,
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <div>Auth</div>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={onChange}
+          />
+          <input
+            type="submit"
+            value={newAccoutn ? 'Create Account' : 'Log In'}
+          />
+        </form>
+        <div>
+          <button>Continue with Google</button>
+          <button>Continue with GitHub</button>
+        </div>
+      </div>
     </>
   );
 };
